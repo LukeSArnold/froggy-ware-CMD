35cbb92f812743aa5e575df520430c4d198a055f
Luke Arnold <luke.arnold@Luke-Arnold-C02V90P1HTD6.local>
2023-10-08 16:32:36 -0600

    added additional logic to handle music video audio

--- engine/youtube_engine.py
+++ engine/youtube_engine.py
@@ -6,17 +6,66 @@ class YoutubeEngine:
 	def __init__(self, log_bool = False):
 		self.logging = log_bool
 	
-	def search_from_list(self, songs):
+	def search_from_list(self, songs, lyrics_correction = False):
 		song_info = []
 		for song in songs:
 			#assuming list of songs is passed in from spotify_engine
 			artist = song['artist']
 			track = song['track']
+			searchString = "{} {} ".format(track,artist)	
+			videosSearch = VideosSearch(searchString, limit = 2)
+			title = ((((videosSearch.result())['result'])[0])['title']).lower()
 
-			searchString = "{} {} lyrics".format(track,artist)	
-			videosSearch = VideosSearch(searchString, limit = 1)
-			link = (((videosSearch.result())['result'])[0])['link']
-			
+			if self.logging:
+				print(title)
+
+			#check to see if video is in title, trying to filter out music videos
+			if "video" not in title:
+				link = (((videosSearch.result())['result'])[0])['link']
+
+			else:
+				if lyrics_correction:
+					searchString = "{} {} {}".format(track,artist,"lyrics")
+					videosSearch = VideosSearch(searchString, limit = 1)
+					alternate_song = (((videosSearch.result())['result'])[0])['title'].lower()					
+
+					if self.logging:
+						print(f"{title} was a music video, because lyrics is enable, using {alternate_song} instead")
+
+					link = (((videosSearch.result())['result'])[0])['link']
+				else:
+					alternate_song = (((videosSearch.result())['result'])[1])['title'].lower()
+
+					if track.lower() in alternate_song:
+
+					
+						#check to see if the song name itself has video in the name
+						if "video" not in track.lower():
+							#music video filtered, take second youtube results
+						
+							if self.logging:
+								print(f"{title} was a music video, using {alternate_song} instead")
+						
+							link = (((videosSearch.result())['result'])[1])['link']		
+
+						elif "music video" not in title:
+							#"video" included in song name, check to see if it is also a youtube video
+							link = (((videosSearch.result())['result'])[0])['link']
+
+						else:
+							#video is included in track name, and first result is a music video. Filter
+
+							if self.logging:
+								print(f"{title} was a music video, using {alternate_song} instead")
+							link = (((videosSearch.result())['result'])[1])['link']
+
+					else:
+						if self.logging:
+							print(f"The search {title} is possibly a music video, but the second result {alternate_song} wasn't the same song. Converting the first link and taking chances")
+
+						link = (((videosSearch.result())['result'])[0])['link']
+						
+
 			track = {'link':link, 'artist':artist, 'track':track}
 			song_info.append(track)
 			
--- main.py
+++ main.py
@@ -9,82 +9,83 @@ import random
 
 
 
-try:
+#try:
 
-	dpg.create_context()
+dpg.create_context()
 
-	def callback(sender, app_data):
-		print('OK was clicked.')
-		print("Sender: ", sender)
-		print("App Data: ", app_data)
-		dpg.set_value(folder, app_data['file_path_name'])
+def callback(sender, app_data):
+	print('OK was clicked.')
+	print("Sender: ", sender)
+	print("App Data: ", app_data)
+	dpg.set_value(folder, app_data['file_path_name'])
+	
+def cancel_callback(sender, app_data):
+	print('Cancel was clicked.')
+	print("Sender: ", sender)
+	print("App Data: ", app_data)
+
+def reset(sender, app_data):
+	desktop_path = os.path.expanduser("~/Desktop")
+	dpg.set_value(folder, desktop_path+ "/MyPlaylist")
+
+def convert(sender, app_data):
+
+	try:	
+		dpg.set_value(status_string, "...RUNNING")
+
+		playlist_url = dpg.get_value(spotify_url) 
+		engine = SpotifyEngine(playlist_url)
+		youtube_engine = YoutubeEngine()
+
+		directory = dpg.get_value(folder)
+
+		dpg.set_value(status_string, "...GETTING SPOTIFY SONGS")
+		songs = engine.get_playlists()
+		dpg.set_value(status_string, "...SONGS ACQUIRED")
+
+		dpg.set_value(status_string, "...FINDING SONGS ON YOUTUBE")
+
+		youtube_links = youtube_engine.search_from_list(songs, True)
+
+
+		dpg.set_value(status_string, "...CONVERTING SONGS")
+
+
+		if os.path.exists(directory):
+			directory = directory + (str(time.asctime())).replace(" ","_")
+			os.mkdir(directory)
+		else:
+			os.mkdir(directory)
 		
-	def cancel_callback(sender, app_data):
-		print('Cancel was clicked.')
-		print("Sender: ", sender)
-		print("App Data: ", app_data)
+		for song in youtube_links:
+			youtube_engine.download_track(song['link'], song['artist'], song['track'], directory)
+			dpg.set_value(status_string, "...CONVERTED {} BY {}".format(song['track'],song['artist']))
 
-	def reset(sender, app_data):
-		desktop_path = os.path.expanduser("~/Desktop")
-		dpg.set_value(folder, desktop_path+ "/MyPlaylist")
+		dpg.set_value(status_string, "All Done!")
+		time.sleep(2)
+		dpg.set_value(status_string, "")
+	except Exception as error:
+		print(error)
+		dpg.set_value(status_string, "Something went wrong, is your playlist public?")
 
-	def convert(sender, app_data):
-
-		try:	
-			dpg.set_value(status_string, "...RUNNING")
-
-			playlist_url = dpg.get_value(spotify_url) 
-			engine = SpotifyEngine(playlist_url)
-			youtube_engine = YoutubeEngine()
-
-			directory = dpg.get_value(folder)
-
-			dpg.set_value(status_string, "...GETTING SPOTIFY SONGS")
-			songs = engine.get_playlists()
-			dpg.set_value(status_string, "...SONGS ACQUIRED")
-
-			dpg.set_value(status_string, "...FINDING SONGS ON YOUTUBE")
-
-			youtube_links = youtube_engine.search_from_list(songs)
-
-
-			dpg.set_value(status_string, "...CONVERTING SONGS")
-
-
-			if os.path.exists(directory):
-				directory = directory + (str(time.asctime())).replace(" ","_")
-				os.mkdir(directory)
-			else:
-				os.mkdir(directory)
-			
-			for song in youtube_links:
-				youtube_engine.download_track(song['link'], song['artist'], song['track'], directory)
-				dpg.set_value(status_string, "...CONVERTED {} BY {}".format(song['track'],song['artist']))
-
-			dpg.set_value(status_string, "All Done!")
-			time.sleep(2)
-			dpg.set_value(status_string, "")
-		except:
-			dpg.set_value(status_string, "Something went wrong, is your playlist public?")
-
-	with dpg.window(tag="Primary Window"):
-		dpg.add_text("Luke's Spotify Converter")
-		spotify_url = dpg.add_input_text(label="Spotify Playlsit URL")
-		desktop_path = os.path.expanduser("~/Desktop")
-		folder = dpg.add_input_text(label="Folder to save", default_value=desktop_path+"/MyPlaylist")
-		status_string = dpg.add_input_text(label="STATUS", default_value="")
-		dpg.add_button(label="CONVERT", callback = convert)
-		dpg.add_file_dialog(
-			directory_selector=True, show=False, callback=callback, tag="file_dialog_id",
-			cancel_callback=cancel_callback, width=700 ,height=400)
-		
-		dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
-		dpg.add_button(label="Reset Directory", callback = reset)
-	dpg.create_viewport(title="Lukey's Spotify Converter", width=800, height=500)
-	dpg.setup_dearpygui()
-	dpg.show_viewport()
-	dpg.set_primary_window("Primary Window", True)
-	dpg.start_dearpygui()
-	dpg.destroy_context()
-except:
-	pass
+with dpg.window(tag="Primary Window"):
+	dpg.add_text("Luke's Spotify Converter")
+	spotify_url = dpg.add_input_text(label="Spotify Playlsit URL")
+	desktop_path = os.path.expanduser("~/Desktop")
+	folder = dpg.add_input_text(label="Folder to save", default_value=desktop_path+"/MyPlaylist")
+	status_string = dpg.add_input_text(label="STATUS", default_value="")
+	dpg.add_button(label="CONVERT", callback = convert)
+	dpg.add_file_dialog(
+		directory_selector=True, show=False, callback=callback, tag="file_dialog_id",
+		cancel_callback=cancel_callback, width=700 ,height=400)
+	
+	dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
+	dpg.add_button(label="Reset Directory", callback = reset)
+dpg.create_viewport(title="Lukey's Spotify Converter", width=800, height=500)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
+#except Exception as error:
+#	print("An error occured: "+error)
