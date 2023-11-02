46e42a88f69bf31e137bd9461d75b0d309ba79c3
Luke Arnold <luke.arnold@Luke-Arnold-C02V90P1HTD6.local>
2023-11-02 17:51:44 -0600

    added silence ffmpeg log option

--- engine/scrubber.py
+++ engine/scrubber.py
@@ -19,7 +19,12 @@ def add_metadata(file_name, artist, title, album = None):
 	
 	mp3_file.tag.save()
 
-def fix_file(file_name, directory):
-        subprocess.run(["ffmpeg","-i", file_name, "-acodec", "copy", f"{directory}/file_fixed.mp3"])
-        os.remove(file_name)
-        os.rename(f"{directory}/file_fixed.mp3", file_name),
+def fix_file(file_name, directory, silence_log = True):
+	
+	if silence_log:
+		subprocess.run(["ffmpeg","-loglevel","quiet", "-i", file_name, "-acodec", "copy", f"{directory}/file_fixed.mp3"])
+	else:
+		subprocess.run(["ffmpeg","-i", file_name, "-acodec", "copy", f"{directory}/file_fixed.mp3"])
+
+	os.remove(file_name)
+	os.rename(f"{directory}/file_fixed.mp3", file_name),
